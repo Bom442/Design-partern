d0b01d3f9192c09686e0f06cbbb11dfbe3326d28
frankLiang <frankliang442@gmail.com>
2022-11-22 16:02:21 +0800

    迭代器模式

---
+++ src/main/java/Iterator/Main.java
@@ -0,0 +1,18 @@
+package Iterator;
+
+public class Main {
+
+    public static void main(String[] args) {
+        BookShelf bookShelf = new BookShelf(4);
+        bookShelf.appendBook(new Book("红楼梦"));
+        bookShelf.appendBook(new Book("西游记"));
+        bookShelf.appendBook(new Book("水浒传"));
+        bookShelf.appendBook(new Book("三国演义"));
+        Iterator iterator = bookShelf.iterator();
+        while (iterator.hasNext()) {
+            Book next = (Book) iterator.next();
+            System.out.println(next.getName());
+        }
+    }
+
+}
